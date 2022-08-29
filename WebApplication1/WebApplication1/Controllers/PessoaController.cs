ee5cbe5810f9ec7f9b0c97284ad0d62986a71d4e
NFontanezi <104529547+NFontanezi@users.noreply.github.com>
2022-08-29 16:47:07 -0300

    Add project files.

---
+++ WebApplication1/WebApplication1/Controllers/PessoaController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PessoaController : ControllerBase
+    {
+        public static Pessoa p1 = new Pessoa("Ana Souza", "222.222.222", "1988-09-18");
+        public static Pessoa p2 = new Pessoa("Bruno Rosa", "111.111.111", "1980-05-10");
+        public static Pessoa p3 = new Pessoa("Paula Torres", "333.333.333", "2003-10-18");
+
+        public static List<Pessoa> pessoas = new List<Pessoa>() { p1, p2, p3 };
+
+        [HttpGet]
+        public List<Pessoa> Listar()
+        {
+            return pessoas;
+        }
+
+        [HttpPost]
+        public void Adicionar(string nome, string cpf, string data)
+        {
+            pessoas.Add(new Pessoa (nome, cpf, data));
+
+        }
+
+        [HttpPut]
+        public Pessoa Alterar(int i, Pessoa p)
+        {
+            pessoas[i] = p;
+            return pessoas[i];
+        }
+
+        [HttpDelete]
+        public List<Pessoa> Deletar(int i)
+        {
+            Pessoa p  = pessoas.First(p => p.Id.Equals(i));
+            pessoas.Remove(p);
+
+            return pessoas;
+        }
+
+
+
+    }
+}
+
+
+
---
+++ WebApplication1/WebApplication1/Pessoa.cs
@@ -0,0 +1,32 @@
+﻿namespace WebApplication1
+{
+    public class Pessoa
+    {
+        public static int id_value = 1;
+        public int Id { get; private set; }
+        public string Nome { get; set; }
+       
+        public string Cpf { get; set; }
+
+        public DateTime DataNasc { get; set; }
+
+        public int Idade { get; private set; }
+
+        public static List<Pessoa> pessoas = new List<Pessoa>();
+
+        public Pessoa(string nome, string cpf, string data)
+        {
+            Id = id_value++;
+            Nome = nome;
+            Cpf = cpf;
+            DataNasc = Convert.ToDateTime(data);//.CultureInfo.InvariantCulture;
+            Idade = DateTime.Now.Year - DataNasc.Year;
+        }
+
+        public static void AddPessoa(Pessoa pessoa)
+        {
+            pessoas.Add(pessoa);
+        }
+
+    }
+}
